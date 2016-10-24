f1ee2c5f73d523bc720b27f6cd590df25795ae28
Joshua Baert <jbghoring@gmail.com>
2016-10-24 12:20:16 -0600

    Skeleton

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 /**
  * Created by Joshua Baert on 10/24/2016.
  */
+
+angular.module('friendList',[]);
--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -1,3 +1,11 @@
 /**
  * Created by Joshua Baert on 10/24/2016.
  */
+
+angular.module('friendList').controller('mainCtrl', function ($scope) {
+    $scope.friends = ['Jordyn', 'Ryan', 'Chelsey'];
+
+    $scope.addFriend = function(newfriend) {
+        $scope.friends.push(newfriend);
+    }
+});
